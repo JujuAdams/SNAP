931ffdf1bc42c3b1a9033138e2fab4f94e70a9d8
Juju Adams <julian.adams@email.com>
2026-05-21 18:47:27 +0100

    9.0 docs

--- config.js
+++ config.js
@@ -21,9 +21,9 @@
 
 const config = {
 	name: 'SNAP',
-	description: 'Data format conversion for GameMaker LTS 2022',
-	latestVersion: '8.2',
-	otherVersions: ['8.1', '7.0', '6.1', '6.0','5.9', '5.8', '5.7', '5.6', '5.5', '5.4', '5.3', '5.2', '5.1', '5.0'],
+	description: 'Data format conversion for GameMaker LTS 2026',
+	latestVersion: '9.0',
+	otherVersions: ['8.2', '8.1', '7.0', '6.1', '6.0','5.9', '5.8', '5.7', '5.6', '5.5', '5.4', '5.3', '5.2', '5.1', '5.0'],
 	favicon: 'assets/favicon.ico',
 	themeColor: '#4f98ff',
 };
